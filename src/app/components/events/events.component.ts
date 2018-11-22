bbad45f6c362b466a43bd9a3122fc7cfdda0e152
Dennis Riungu <denpalrius@gmail.com>
2018-11-22 08:41:32 +0300

    Validated new event form

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -24,16 +24,20 @@ export class EventsComponent implements OnInit {
     private readonly snackBar: MatSnackBar,
     private readonly fb: FormBuilder,
   ) {
-    this.recepients = [];
+    // this.recepients = [
+    //   { name: 'Anna-Marie Silvester', email: 'v-annsil@microsoft.com' },
+    //   { name: 'Charles Wahome', email: 'v-chgita@microsoft.com' },
+    //   { name: 'Marvin Ochieng', email: 'v-edmarv@microsoft.com' },
+    //   { name: 'Duncan Okwako', email: 'v-duokwa@microsoft.com' },
+    // ];
+    this.recepients = [
+      { name: 'Dennor', email: 'denpalrius@gmail.com' },
+      { name: 'Mzitoh', email: 'riungu@quantumfig.com' },
+    ];
   }
 
   ngOnInit() {
     this.prepareForm();
-  }
-
-  private getContent() {
-    // tslint:disable-next-line:max-line-length
-    return `<!doctypehtml><html><head><metaname="viewport"content="width=device-width"><metahttp-equiv="Content-Type"content="text/html;charset=UTF-8"><title>SimpleTransactionalEmail</title><style>/*-------------------------------------INLINEDWITHhtmlemail.io/inline-------------------------------------*//*-------------------------------------RESPONSIVEANDMOBILEFRIENDLYSTYLES-------------------------------------*/@mediaonlyscreenand(max-width:620px){table[class=body]h1{font-size:28px!important;margin-bottom:10px!important;}table[class=body]p,table[class=body]ul,table[class=body]ol,table[class=body]td,table[class=body]span,table[class=body]a{font-size:16px!important;}table[class=body].wrapper,table[class=body].article{padding:10px!important;}table[class=body].content{padding:0!important;}table[class=body].container{padding:0!important;width:100%!important;}table[class=body].main{border-left-width:0!important;border-radius:0!important;border-right-width:0!important;}table[class=body].btntable{width:100%!important;}table[class=body].btna{width:100%!important;}table[class=body].img-responsive{height:auto!important;max-width:100%!important;width:auto!important;}}/*-------------------------------------PRESERVETHESESTYLESINTHEHEAD-------------------------------------*/@mediaall{.ExternalClass{width:100%;}.ExternalClass,.ExternalClassp,.ExternalClassspan,.ExternalClassfont,.ExternalClasstd,.ExternalClassdiv{line-height:100%;}.apple-linka{color:inherit!important;font-family:inherit!important;font-size:inherit!important;font-weight:inherit!important;line-height:inherit!important;text-decoration:none!important;}.btn-primarytabletd:hover{background-color:#34495e!important;}.btn-primarya:hover{background-color:#34495e!important;border-color:#34495e!important;}}</style></head><bodyclass=""style="background-color:#f6f6f6;font-family:sans-serif;-webkit-font-smoothing:antialiased;font-size:14px;line-height:1.4;margin:0;padding:0;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;"><tableborder="0"cellpadding="0"cellspacing="0"class="body"style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;background-color:#f6f6f6;"><tr><tdstyle="font-family:sans-serif;font-size:14px;vertical-align:top;">&nbsp;</td><tdclass="container"style="font-family:sans-serif;font-size:14px;vertical-align:top;display:block;Margin:0auto;max-width:580px;padding:10px;width:580px;"><divclass="content"style="box-sizing:border-box;display:block;Margin:0auto;max-width:580px;padding:10px;"><!--STARTCENTEREDWHITECONTAINER--><spanclass="preheader"style="color:transparent;display:none;height:0;max-height:0;max-width:0;opacity:0;overflow:hidden;mso-hide:all;visibility:hidden;width:0;">Thisispreheadertext.Someclientswillshowthistextasapreview.</span><tableclass="main"style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;background:#ffffff;border-radius:3px;"><!--STARTMAINCONTENTAREA--><tr><tdclass="wrapper"style="font-family:sans-serif;font-size:14px;vertical-align:top;box-sizing:border-box;padding:20px;"><tableborder="0"cellpadding="0"cellspacing="0"style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;"><tr><tdstyle="font-family:sans-serif;font-size:14px;vertical-align:top;"><pstyle="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Hithere,</p><pstyle="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">SometimesyoujustwanttosendasimpleHTMLemailwithasimpledesignandclearcalltoaction.Thisisit.</p><tableborder="0"cellpadding="0"cellspacing="0"class="btnbtn-primary"style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;box-sizing:border-box;"><tbody><tr><tdalign="left"style="font-family:sans-serif;font-size:14px;vertical-align:top;padding-bottom:15px;"><tableborder="0"cellpadding="0"cellspacing="0"style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:auto;"><tbody><tr><tdstyle="font-family:sans-serif;font-size:14px;vertical-align:top;background-color:#3498db;border-radius:5px;text-align:center;"><ahref="http://htmlemail.io"target="_blank"style="display:inline-block;color:#ffffff;background-color:#3498db;border:solid1px#3498db;border-radius:5px;box-sizing:border-box;cursor:pointer;text-decoration:none;font-size:14px;font-weight:bold;margin:0;padding:12px25px;text-transform:capitalize;border-color:#3498db;">CallToAction</a></td></tr></tbody></table></td></tr></tbody></table><pstyle="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Thisisareallysimpleemailtemplate.Itssolepurposeistogettherecipienttoclickthebuttonwithnodistractions.</p><pstyle="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Goodluck!Hopeitworks.</p></td></tr></table></td></tr><!--ENDMAINCONTENTAREA--></table><!--STARTFOOTER--><divclass="footer"style="clear:both;Margin-top:10px;text-align:center;width:100%;"><tableborder="0"cellpadding="0"cellspacing="0"style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;"><tr><tdclass="content-block"style="font-family:sans-serif;vertical-align:top;padding-bottom:10px;padding-top:10px;font-size:12px;color:#999999;text-align:center;"><spanclass="apple-link"style="color:#999999;font-size:12px;text-align:center;">CompanyInc,3AbbeyRoad,SanFranciscoCA94102</span><br>Don'tliketheseemails?<ahref="http://i.imgur.com/CScmqnj.gif"style="text-decoration:underline;color:#999999;font-size:12px;text-align:center;">Unsubscribe</a>.</td></tr><tr><tdclass="content-blockpowered-by"style="font-family:sans-serif;vertical-align:top;padding-bottom:10px;padding-top:10px;font-size:12px;color:#999999;text-align:center;">Poweredby<ahref="http://htmlemail.io"style="color:#999999;font-size:12px;text-align:center;text-decoration:none;">HTMLemail</a>.</td></tr></table></div><!--ENDFOOTER--><!--ENDCENTEREDWHITECONTAINER--></div></td><tdstyle="font-family:sans-serif;font-size:14px;vertical-align:top;">&nbsp;</td></tr></table></body></html>`;
   }
 
   prepareForm() {
@@ -60,7 +64,8 @@ export class EventsComponent implements OnInit {
   onSubmitRecepient() {
     if (this.newRecepientForm.valid) {
       const recepientDetails = this.newRecepientForm.value;
-      if (this.recepients.indexOf(recepientDetails) > -1) {
+
+      if (this.recepients.indexOf(recepientDetails) === -1) {
         this.recepients.push({
           name: recepientDetails.name,
           email: recepientDetails.email,
@@ -68,6 +73,8 @@ export class EventsComponent implements OnInit {
 
         this.newRecepientForm.reset();
       }
+    } else {
+      this.newRecepientForm.markAsDirty();
     }
   }
 
@@ -82,11 +89,16 @@ export class EventsComponent implements OnInit {
 
       this.graphService.createEvent(event).subscribe(res => {
         if (res) {
-          this.showSnackBar(`Event: ${res.subject} created successfully`);
+          this.newEmailForm.reset();
+          this.recepients = [];
+
+          this.showSnackBar(`Event: ${res.subject} was sent successfully`);
         } else {
           this.showSnackBar(`The event was not created :( `);
         }
       });
+    } else {
+      this.newEmailForm.markAsDirty();
     }
   }
 
@@ -126,16 +138,20 @@ export class EventsComponent implements OnInit {
         content: body,
       },
       start: {
-        dateTime: moment().format(),
+        dateTime: moment()
+          .add(20, 'd')
+          .format(),
         timeZone: 'Africa/Nairobi',
       },
       end: {
-        dateTime: moment().format(),
+        dateTime: moment()
+          .add(21, 'd')
+          .format(),
         timeZone: 'Africa/Nairobi',
       },
       attendees: attendees,
       location: {
-        displayName: 'Camp, Nou, Barcelona, Spain',
+        displayName: 'The Oval, Westlands, Nairobi, Kenya',
         locationType: 'default',
       },
     };
